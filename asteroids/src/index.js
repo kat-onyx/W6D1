91977a56168cf3fef40c3e1847685bb70f7ff008
kat-onyx <sarah.kathryn.peters@gmail.com>
2018-11-26 16:41:10 -0800

    Finish MovingObject in/for phase 2 in/for asteroids

--- asteroids/src/index.js
+++ asteroids/src/index.js
@@ -1,1 +1,11 @@
+const MovingObject = require("./moving_object.js");
 
+window.MovingObject = MovingObject;
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    let canvas = document.getElementById("game-canvas");
+    let ctx = canvas.getContext("2d");
+    // let s = new MovingObject({pos: [100, 100], vel: [0, 0], radius: 20, color: "#00ff00"});
+    // s.draw(ctx);
+    debugger
+  });
