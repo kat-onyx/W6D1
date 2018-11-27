23d267e6d17b620987b82f9edff017c29adb5885
kat-onyx <sarah.kathryn.peters@gmail.com>
2018-11-26 16:13:28 -0800

    Complete phase 0 and phase 1 of asteroids

--- inheritance_exercises/inheritance.js
+++ inheritance_exercises/inheritance.js
@@ -1,32 +1,6 @@
-
-Function.prototype.inherits = function(superClass) {
-  this.prototype = Object.create(superClass.prototype);
-  this.prototype.constructor = this;
-};
-
-function MovingObject () {}
-
-MovingObject.prototype.woosh = function() {
-  console.log("woosh!");
-};
-
-function Ship () {}
-Ship.inherits(MovingObject);
-
-function Asteroid () {}
-Asteroid.inherits(MovingObject);
-
-Asteroid.prototype.blowUp = function() {
-  console.log("Aerosmith is playing");
-};
-
-let s = new Ship();
-let a = new Asteroid();
-
+//
 // Function.prototype.inherits = function(superClass) {
-//   function Surrogate() {}
-//   Surrogate.prototype = superClass.prototype;
-//   this.prototype = new Surrogate();
+//   this.prototype = Object.create(superClass.prototype);
 //   this.prototype.constructor = this;
 // };
 //
@@ -47,3 +21,29 @@ let a = new Asteroid();
 // };
 //
 // let s = new Ship();
+// let a = new Asteroid();
+
+Function.prototype.inherits = function(superClass) {
+  function Surrogate() {}
+  Surrogate.prototype = superClass.prototype;
+  this.prototype = new Surrogate();
+  this.prototype.constructor = this;
+};
+
+function MovingObject () {}
+
+MovingObject.prototype.woosh = function() {
+  console.log("woosh!");
+};
+
+function Ship () {}
+Ship.inherits(MovingObject);
+
+function Asteroid () {}
+Asteroid.inherits(MovingObject);
+
+Asteroid.prototype.blowUp = function() {
+  console.log("Aerosmith is playing");
+};
+
+let s = new Ship();
